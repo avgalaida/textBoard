8938b3beb5b9414d7fd7bc707d1c866d6184ee3b
Alexandr Galaida <evaaxelrod@yandex.ru>
2022-10-07 15:17:54 +0500

    Создан интерфейс поискового репозитория.

---
+++ search/repository.go
@@ -0,0 +1,30 @@
+package search
+
+import (
+	"context"
+	"textBoard/schema"
+)
+
+type Repository interface {
+	Close()
+	InsertPost(ctx context.Context, post schema.Post) error
+	SearchPosts(ctx context.Context, query string, skip uint64, take uint64) ([]schema.Post, error)
+}
+
+var impl Repository
+
+func SetRepository(repository Repository) {
+	impl = repository
+}
+
+func Close() {
+	impl.Close()
+}
+
+func InsertPost(ctx context.Context, post schema.Post) error {
+	return impl.InsertPost(ctx, post)
+}
+
+func SearchPosts(ctx context.Context, query string, skip uint64, take uint64) ([]schema.Post, error) {
+	return impl.SearchPosts(ctx, query, skip, take)
+}
