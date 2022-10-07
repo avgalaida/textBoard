104a7278560a7f1902ea7443641314b08c3f80a9
Alexandr Galaida <evaaxelrod@yandex.ru>
2022-10-07 15:09:34 +0500

    Добавлена реализация функций подписок.

--- event/nats.go
+++ event/nats.go
@@ -48,3 +48,40 @@ func (mq *NatsEventStore) writeMessage(m Message) ([]byte, error) {
 	}
 	return b.Bytes(), nil
 }
+
+func (e *NatsEventStore) OnPostCreated(f func(PostCreatedMessage)) (err error) {
+	m := PostCreatedMessage{}
+	e.postCreatedSubscription, err = e.nc.Subscribe(m.Key(), func(msg *nats.Msg) {
+		e.readMessage(msg.Data, &m)
+		f(m)
+	})
+	return
+}
+
+func (mq *NatsEventStore) readMessage(data []byte, m interface{}) error {
+	b := bytes.Buffer{}
+	b.Write(data)
+	return gob.NewDecoder(&b).Decode(m)
+}
+
+func (e *NatsEventStore) SubscribePostCreated() (<-chan PostCreatedMessage, error) {
+	m := PostCreatedMessage{}
+	e.postCreatedChan = make(chan PostCreatedMessage, 64)
+	ch := make(chan *nats.Msg, 64)
+	var err error
+	e.postCreatedSubscription, err = e.nc.ChanSubscribe(m.Key(), ch)
+	if err != nil {
+		return nil, err
+	}
+	// Decode message
+	go func() {
+		for {
+			select {
+			case msg := <-ch:
+				e.readMessage(msg.Data, &m)
+				e.postCreatedChan <- m
+			}
+		}
+	}()
+	return (<-chan PostCreatedMessage)(e.postCreatedChan), nil
+}
