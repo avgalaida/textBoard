898b50e77e00b2c84a862d5455cfb11f7721f788
Alexandr Galaida <evaaxelrod@yandex.ru>
2022-10-12 01:28:14 +0500

    Добавлено логгирование.

--- event/nats.go
+++ event/nats.go
@@ -5,6 +5,7 @@ import (
 	"encoding/gob"
 	"github.com/avgalaida/textBoard/schema"
 	"github.com/nats-io/nats.go"
+	"log"
 )
 
 type NatsEventStore struct {
@@ -21,55 +22,12 @@ func NewNats(url string) (*NatsEventStore, error) {
 	return &NatsEventStore{nc: nc}, nil
 }
 
-func (e *NatsEventStore) Close() {
-	if e.nc != nil {
-		e.nc.Close()
-	}
-	if e.postCreatedSubscription != nil {
-		e.postCreatedSubscription.Unsubscribe()
-	}
-	close(e.postCreatedChan)
-}
-
-func (e *NatsEventStore) PublishPostCreated(post schema.Post) error {
-	m := PostCreatedMessage{post.ID, post.Body, post.CreatedAt}
-	data, err := e.writeMessage(&m)
-	if err != nil {
-		return err
-	}
-	return e.nc.Publish(m.Key(), data)
-}
-
-func (mq *NatsEventStore) writeMessage(m Message) ([]byte, error) {
-	b := bytes.Buffer{}
-	err := gob.NewEncoder(&b).Encode(m)
-	if err != nil {
-		return nil, err
-	}
-	return b.Bytes(), nil
-}
-
-func (e *NatsEventStore) OnPostCreated(f func(PostCreatedMessage)) (err error) {
+func (es *NatsEventStore) SubscribePostCreated() (<-chan PostCreatedMessage, error) {
 	m := PostCreatedMessage{}
-	e.postCreatedSubscription, err = e.nc.Subscribe(m.Key(), func(msg *nats.Msg) {
-		e.readMessage(msg.Data, &m)
-		f(m)
-	})
-	return
-}
-
-func (mq *NatsEventStore) readMessage(data []byte, m interface{}) error {
-	b := bytes.Buffer{}
-	b.Write(data)
-	return gob.NewDecoder(&b).Decode(m)
-}
-
-func (e *NatsEventStore) SubscribePostCreated() (<-chan PostCreatedMessage, error) {
-	m := PostCreatedMessage{}
-	e.postCreatedChan = make(chan PostCreatedMessage, 64)
+	es.postCreatedChan = make(chan PostCreatedMessage, 64)
 	ch := make(chan *nats.Msg, 64)
 	var err error
-	e.postCreatedSubscription, err = e.nc.ChanSubscribe(m.Key(), ch)
+	es.postCreatedSubscription, err = es.nc.ChanSubscribe(m.Key(), ch)
 	if err != nil {
 		return nil, err
 	}
@@ -78,10 +36,59 @@ func (e *NatsEventStore) SubscribePostCreated() (<-chan PostCreatedMessage, erro
 		for {
 			select {
 			case msg := <-ch:
-				e.readMessage(msg.Data, &m)
-				e.postCreatedChan <- m
+				if err := es.readMessage(msg.Data, &m); err != nil {
+					log.Fatal(err)
+				}
+				es.postCreatedChan <- m
 			}
 		}
 	}()
-	return (<-chan PostCreatedMessage)(e.postCreatedChan), nil
+	return (<-chan PostCreatedMessage)(es.postCreatedChan), nil
+}
+
+func (es *NatsEventStore) OnPostCreated(f func(PostCreatedMessage)) (err error) {
+	m := PostCreatedMessage{}
+	es.postCreatedSubscription, err = es.nc.Subscribe(m.Key(), func(msg *nats.Msg) {
+		if err := es.readMessage(msg.Data, &m); err != nil {
+			log.Fatal(err)
+		}
+		f(m)
+	})
+	return
+}
+
+func (es *NatsEventStore) Close() {
+	if es.nc != nil {
+		es.nc.Close()
+	}
+	if es.postCreatedSubscription != nil {
+		if err := es.postCreatedSubscription.Unsubscribe(); err != nil {
+			log.Fatal(err)
+		}
+	}
+	close(es.postCreatedChan)
+}
+
+func (es *NatsEventStore) PublishPostCreated(post schema.Post) error {
+	m := PostCreatedMessage{post.ID, post.Body, post.CreatedAt}
+	data, err := es.writeMessage(&m)
+	if err != nil {
+		return err
+	}
+	return es.nc.Publish(m.Key(), data)
+}
+
+func (es *NatsEventStore) writeMessage(m Message) ([]byte, error) {
+	b := bytes.Buffer{}
+	err := gob.NewEncoder(&b).Encode(m)
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+func (es *NatsEventStore) readMessage(data []byte, m interface{}) error {
+	b := bytes.Buffer{}
+	b.Write(data)
+	return gob.NewDecoder(&b).Decode(m)
 }
