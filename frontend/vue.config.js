d8298c49233b74d6033bf89113fa2774dfc87108
Alexandr Galaida <evaaxelrod@yandex.ru>
2022-10-14 05:41:25 +0500

    Улучшен конфиг.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   devServer: {
     port: 3000,
   },
-};
+  transpileDependencies: true
+})
